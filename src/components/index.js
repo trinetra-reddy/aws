d0ddddae945e5fcd9933e65d94f320e4f13a7bb9
giridhar <gonuguntlagirish@gmail.com>
2023-02-09 18:15:34 +0530

    commited code

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Banner from "./Banner/Banner";
 import Carousel from "./Carousel/Carousel";
+import ExpertsService from "./ExpertsService/ExpertsService";
+import Casestudies from "./Casestudies/Casestudies";
 
-export { Header, Banner,Carousel, Footer };
+export { Header, Banner,Carousel,ExpertsService,Casestudies, Footer };
