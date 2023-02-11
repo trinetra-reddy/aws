5ec58efe6f08e4319acc8576447edad7e0a9c9b0
Raavi Haritha <haritha4682@gmail.com>
2023-02-11 19:41:38 +0530

    Three components doe in about us

---
+++ src/components/AboutusBanner/AboutusBanner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./AboutusBanner.scss";
+const AboutusBanner = () => {
+  return (
+    <>
+      <div className="AboutusBanner-main">
+        <div className="AboutusBanner-headerwrapper">
+          <h1 className="AboutusBanner-header">About Us</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+export default AboutusBanner;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Banner from "./Banner/Banner";
 import Carousel from "./Carousel/Carousel";
+import YearsofExperience from "./YearsofExperience/YearsofExperience";
+import AboutusBanner from "./AboutusBanner/AboutusBanner";
+import VissionMission from "./VissionMission/VissionMission";
 
-export { Header, Banner,Carousel, Footer };
+export { Header, Banner,Carousel, Footer,AboutusBanner,VissionMission,YearsofExperience};
